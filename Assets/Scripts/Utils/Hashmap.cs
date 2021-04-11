001a5b5ebce7ced012823903411f3a5cbd8d783c
ProperNovice <ProperNovice@users.noreply.github.com>
2021-04-11 11:59:27 +0300

    q

--- Assets/Scripts/Utils/Hashmap.cs
+++ Assets/Scripts/Utils/Hashmap.cs
@@ -2,7 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hashmap
+public class Hashmap<K, V>
 {
+
+    private ArrayList<K> keys = new ArrayList<K>();
+    private ArrayList<V> values = new ArrayList<V>();
+
+    public Hashmap()
+    {
+
+    }
+
+    public void put(K key, V value)
+    {
+        this.keys.addLast(key);
+        this.values.addLast(value);
+    }
 
 }
