789d536d3501294aeb021f226bdcb84f2f3d50a4
ProperNovice <ProperNovice@users.noreply.github.com>
2021-04-16 00:04:28 +0300

    q

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -42,16 +42,6 @@ public class Test : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            ManagerList.INSTANCE.lists.getValue(EList.DECK).animateAsynchronous();
-
-        else if (Input.GetMouseButtonDown(1))
-        {
-
-            foreach (GameObject gameObject in ManagerList.INSTANCE.lists.getValue(EList.DECK).arrayList)
-                Logger.log(gameObject.GetComponent<SpriteView>().getCoordinatesTopLeft());
-
-        }
 
     }
 
