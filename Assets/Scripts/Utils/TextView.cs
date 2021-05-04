c3ad2f405606d3620c7bd19cb6b87934d590a8ae
ProperNovice <ProperNovice@users.noreply.github.com>
2021-05-04 23:06:23 +0300

    q

--- Assets/Scripts/Utils/TextView.cs
+++ Assets/Scripts/Utils/TextView.cs
@@ -43,6 +43,7 @@ public class TextView : MonoBehaviour
     public void setActive(bool value)
     {
         this.gameObject.SetActive(value);
+        OnMouseExit();
     }
 
     public void OnMouseEnter()
