2e24c1919cff90561dce7110d33eb8bab4776653
ProperNovice <ProperNovice@users.noreply.github.com>
2021-05-09 23:23:02 +0300

    Update ManagerSpriteList.cs

--- Assets/Scripts/ManagerSpriteList.cs
+++ Assets/Scripts/ManagerSpriteList.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerSpriteList : MonoBehaviour
+public class ManagerSpriteList : MonoBehaviour, Interfaces.SaveLoadAble
 {
 
     public static ManagerSpriteList INSTANCE;
+    public ArrayList<SpriteList> lists;
 
     public void Awake()
     {
         INSTANCE = this;
+        this.lists = new ArrayList<SpriteList>();
     }
 
+    public void loadStart()
+    {
+        foreach (SpriteList spriteList in this.lists)
+            spriteList.arrayList.loadStart();
+    }
+
+    public void loadState()
+    {
+        foreach (SpriteList spriteList in this.lists)
+            spriteList.arrayList.loadState();
+    }
+
+    public void saveStart()
+    {
+        foreach (SpriteList spriteList in this.lists)
+            spriteList.arrayList.saveStart();
+    }
+
+    public void saveState()
+    {
+        foreach (SpriteList spriteList in this.lists)
+            spriteList.arrayList.saveState();
+    }
 }
