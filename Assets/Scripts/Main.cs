f9adf30fad091ec6bc14a9fc08a242ac72d54529
ProperNovice <ProperNovice@users.noreply.github.com>
2021-05-18 10:38:35 +0300

    q

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,6 +11,19 @@ public class Main : MonoBehaviour
     void Start()
     {
         this.screenResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+
+        instantiates();
+        spriteLists();
+    }
+
+    private void instantiates()
+    {
+
+    }
+
+    private void spriteLists()
+    {
+
     }
 
 }
