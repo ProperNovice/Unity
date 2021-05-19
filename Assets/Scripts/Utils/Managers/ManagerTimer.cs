362dc36d99f20087577593515c047c592071662f
ProperNovice <ProperNovice@users.noreply.github.com>
2021-05-19 10:34:13 +0300

    q

--- Assets/Scripts/Utils/Managers/ManagerTimer.cs
+++ Assets/Scripts/Utils/Managers/ManagerTimer.cs
@@ -5,30 +5,24 @@ using UnityEngine;
 public class ManagerTimer : MonoBehaviour
 {
 
+    public static ManagerTimer INSTANCE;
     private ArrayList<TimeToken> list;
 
     void Awake()
     {
+        INSTANCE = this;
         this.list = new ArrayList<TimeToken>();
     }
 
     void Update()
     {
 
-        float timePassed = Time.deltaTime;
-
         foreach (TimeToken timeToken in this.list.clone())
         {
 
-            timeToken.timePassed(timePassed);
+            timeToken.timePassed(Time.deltaTime);
 
-            if (!timeToken.notify)
-                continue;
-
-            timeToken.notify = false;
-            timeToken.timerAble.notify();
-
-            if (timeToken.timesToNotify == -1)
+            if (timeToken.timesToNotify == -1 || timeToken.timesToNotify > 0)
                 continue;
 
             this.list.remove(timeToken);
@@ -47,21 +41,35 @@ public class ManagerTimer : MonoBehaviour
         this.list.addLast(new TimeToken(millisToNotify, timesToNotify, timerAble));
     }
 
+    public void remove(Interfaces.TimerAble timerAble)
+    {
+
+        foreach (TimeToken timeToken in this.list.clone())
+        {
+
+            if (!timeToken.timerAble.Equals(timerAble))
+                continue;
+
+            this.list.remove(timeToken);
+            break;
+
+        }
+
+    }
+
     private class TimeToken
     {
 
         public float millisToNotify, millisPassed;
         public Interfaces.TimerAble timerAble;
-        public bool notify;
         public int timesToNotify;
 
         public TimeToken(float millisToNotify, int timesToNotify, Interfaces.TimerAble timerAble)
         {
-            this.millisToNotify = millisToNotify;
+            this.millisToNotify = millisToNotify / 1000;
             this.timerAble = timerAble;
             this.millisPassed = 0;
             this.timesToNotify = timesToNotify;
-            this.notify = false;
         }
 
         public void timePassed(float millis)
@@ -72,7 +80,7 @@ public class ManagerTimer : MonoBehaviour
             if (this.millisPassed < this.millisToNotify)
                 return;
 
-            this.notify = true;
+            this.timerAble.notify();
             this.millisPassed -= this.millisToNotify;
 
             if (this.timesToNotify == -1)
